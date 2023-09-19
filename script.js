87d7ee0f6541654b2049e145f75ff7c99bfb5f26
Brock Nelson <Bmnelson87@gmail.com>
2023-09-19 11:04:01 -0500

    working on making button add a row to table- still needs to be debugged.

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 console.log("Hello World!")
+let employeeList = document.querySelector("#employee-table");
+
+function addEmployee(event){
+    event.preventDefault();
+    employeeList.innerHTML += `
+        <tr>
+            <td>First Name</td>
+            <td>Last Name</td>
+            <td>ID</td>
+            <td>Job Title</td>
+            <td>Salary</td>
+        </tr>
+    `
+
+}
