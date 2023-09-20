e886582825b8376e53a26ec68363bf30bc174688
Brock Nelson <Bmnelson87@gmail.com>
2023-09-20 08:36:29 -0500

    added README formatted functions for easier use, displaying salary in currency, still working on removing a deleted row from monthly salary

--- README.md
+++ README.md
@@ -1,9 +1,50 @@
+Additional README details can be found [here](https://github.com/PrimeAcademy/readme-template/blob/master/README.md).
+
 # Project Name
 
-[Project Instructions](./INSTRUCTIONS.md), this line may be removed once you have updated the README.md
+Weekend Salary Calculator
 
 ## Description
 
-Your project description goes here. What problem did you solve? How did you solve it?
+Duration: 1 day
 
-Additional README details can be found [here](https://github.com/PrimeAcademy/readme-template/blob/master/README.md).
+Weekend Salary Calculator inputs employees
+information and stores it in a table.
+
+Their yearly salary is then added together and 
+divided by 12 to return monthly salary below the 
+table.
+
+If the monthly salary is above $20,000 the background
+of the monthly salary will turn red to alert the user 
+of the overage.
+
+## Prerequisits
+
+None.
+
+## Installation
+
+## Usage
+
+1. Enter employee data
+2. If employee is no longer employed push 'delete'
+button to remove.
+
+
+## Built With
+
+Visual Basic
+Git
+Github
+
+## License
+
+## Acknowledgement
+Prime Digital Academy
+
+## Support
+If you have suggestions or issues, please email me at
+bmnelson87@gmail.com
+
+
--- script.js
+++ script.js
@@ -1,56 +1,78 @@
 console.log("Hello World!")
+
 let employeeRow = document.querySelector("#employee-table");
 let monthlySalaryDiv = document.querySelector('#monthly-salary');
 
-let totalSalary = 0;
 
+let employeeIndex = 0;
 
+let salaryArray = [];
 
-//clear out initial inputs.
-    document.querySelector('#first-name').value = '';
-    document.querySelector('#last-name').value = '';
-    document.querySelector('#ID').value = '';
-    document.querySelector('#job-title').value = '';
-    document.querySelector('#salary').value = '';
-    
+let currency = Intl.NumberFormat('en-US',{
+    style: "currency",
+    currency: "USD",
+});
+   
 
 function addEmployee(event){
-    
     let firstName = document.querySelector('#first-name').value;
     let lastName = document.querySelector('#last-name').value;
     let Identification = document.querySelector('#ID').value;
     let jobTitle = document.querySelector('#job-title').value;
     let salary = document.querySelector('#salary').value;
     
-    totalSalary += salary;
-    let monthlySalary = (totalSalary / 12).toFixed(2);
-
     employeeRow.innerHTML += `
         <tr>
             <td>${firstName}</td>
             <td>${lastName}</td>
             <td>${Identification}</td>
             <td>${jobTitle}</td>
-            <td>${salary}</td>
-            <td><button onClick="removeRow(event)">Remove</button></td>
+            <td>${currency.format(salary)}</td>
+            <td><button onClick="removeRow(event,${employeeIndex})">Remove</button></td>
         </tr>
     `
-    monthlySalaryDiv.innerHTML =`
-        <h3>$ ${monthlySalary}</h3>
-    `
+    // update monthly salary
+    monthlySalaryCalc(salary);
+
 
     //clear out inputs
-    document.querySelector('#first-name').value = '';
-    document.querySelector('#last-name').value = '';
-    document.querySelector('#ID').value = '';
-    document.querySelector('#job-title').value = '';
-    document.querySelector('#salary').value = '';
+    clearInputs();
     
     // prefent form from refreshing the page.
     event.preventDefault();
 }
 
-function removeRow(event){
-    event.target.parentElement.parentElement.remove();
+function clearInputs(){
+    document.querySelector('#first-name').value = '';
+    document.querySelector('#last-name').value = '';
+    document.querySelector('#ID').value = '';
+    document.querySelector('#job-title').value = '';
+    document.querySelector('#salary').value = '';
 }
 
+function monthlySalaryCalc(salary){
+    salaryArray.push(salary);
+
+    let totalSalary = 0;
+
+    for(let income of salaryArray){
+        totalSalary += income;
+    }
+
+    let monthlySalary = totalSalary / 12;
+
+    monthlySalaryDiv.innerHTML =`
+        <h3>${currency.format(monthlySalary)}</h3>
+    `
+
+}
+
+function removeRow(event,index){
+    event.target.parentElement.parentElement.remove();
+    salaryArray.splice(index,1);
+    monthlySalaryCalc();
+}
+
+//clear out initial inputs.
+clearInputs();
+ 
